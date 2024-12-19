ee20b48cafc3748186091a8b1d04fda30d7fd568
Guilherme Deluca <gdelucapereira@gmail.com>
2024-12-18 21:31:07 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Guilherme Deluca on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
