87734b288523edef01b7372a06c2cce974487874
Guilherme Deluca <gdelucapereira@gmail.com>
2024-12-24 18:57:50 -0300

    feat: added scrum history

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -38,7 +38,17 @@ struct DetailView: View {
             Section(header: Text("Attendees")) {
                 ForEach(scrum.attendees) { attendee in
                     Label(attendee.name, systemImage: "person")
-
+                }
+            }
+            Section(header: Text("History")) {
+                if scrum.history.isEmpty {
+                    Label("No meeting yet", systemImage: "calendar.badge.exclamationmark")
+                }
+                ForEach(scrum.history) { history in
+                    HStack {
+                        Image(systemName: "calendar")
+                        Text(history.date, style: .date)
+                    }
                 }
             }
         }.navigationTitle(scrum.title).toolbar {
@@ -47,26 +57,26 @@ struct DetailView: View {
                 editingScrum = scrum
             }
         }
-            .sheet(isPresented: $isPresentingEditView) {
-                NavigationStack {
-                    DetailEditView(scrum: $editingScrum)
-                        .navigationTitle(scrum.title)
-                        .toolbar {
-                            ToolbarItem(placement: .cancellationAction) {
-                                Button("Cancel") {
-                                    isPresentingEditView = false
-                                }
-                            }
-                            ToolbarItem(
-                                placement: .confirmationAction
-                            ) {
-                                Button("Done") {
-                                    isPresentingEditView = false
-                                    scrum = editingScrum
-                                }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack {
+                DetailEditView(scrum: $editingScrum)
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
                             }
                         }
-                }
+                        ToolbarItem(
+                            placement: .confirmationAction
+                        ) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                                scrum = editingScrum
+                            }
+                        }
+                    }
+            }
         }
     }
 }
--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @State private var isPresentingNewScrumView = false
 
     var body: some View {
         NavigationStack {
@@ -17,10 +18,17 @@ struct ScrumsView: View {
                     CardView(scrum: scrum)
                 }.listRowBackground(scrum.theme.mainColor)
             }.navigationTitle("Daily Scrums").toolbar {
-                Button(action: {}) {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }) {
                     Image(systemName: "plus")
                 }.accessibilityLabel("New Scrum")
             }
+        }.sheet(isPresented: $isPresentingNewScrumView) {
+            NewScrumSheet(
+                scrums: $scrums,
+                isPresentingNewScrumView: $isPresentingNewScrumView
+            )
         }
     }
 }
